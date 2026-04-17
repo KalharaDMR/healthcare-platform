83a3e6d4e1fde4a3e8681ade8b5bb25882bded62
Hiruni <kathaluwagehiruni@gmail.com>
2026-04-17 17:18:23 +0530

    fix: frontend url fixed

--- healthcare-frontend/src/api/axiosInstance.js
+++ healthcare-frontend/src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8089';
+//const BASE_URL = 'http://localhost:8089';
+const BASE_URL = 'http://healthcare.local';
 
 const api = axios.create({
   baseURL: BASE_URL,
